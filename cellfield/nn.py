46ea0c0189a184c9fa440179e82d09564ca96531
Eugene Obukhov <irvind25@gmail.com>
2023-06-14 06:26:10 +0300

    Refactor annotations

--- cellfield/nn.py
+++ cellfield/nn.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Optional
 import numpy as np
 from numpy.typing import ArrayLike
 
@@ -6,14 +6,14 @@ from numpy.typing import ArrayLike
 class FeedForwardNetwork:
     def __init__(self,
                  input_num: int,
-                 hidden_layer_nums: List[int],
+                 hidden_layer_nums: list[int],
                  output_num: int,
                  hidden_activation: str = 'relu',
                  output_activation: str = 'sigmoid',
                  seed: Optional[int] = None,
-                 w_matrices: Optional[List[ArrayLike]] = None,
-                 b_weights: Optional[List[ArrayLike]] = None):
-        assert (matr_weights and b_weights) or (not matr_weights and not b_weights)
+                 w_matrices: Optional[list[ArrayLike]] = None,
+                 b_weights: Optional[list[ArrayLike]] = None):
+        assert (w_matrices and b_weights) or (not w_matrices and not b_weights)
 
         self.input_num = input_num
         self.hidden_layer_nums = hidden_layer_nums
@@ -22,15 +22,15 @@ class FeedForwardNetwork:
         self.output_activation = output_activation
         self.random_gen = np.random.default_rng(seed)
 
-        if matr_weights:
+        if w_matrices:
             self._init_random_weights()
         else:
-            assert self._is_input_matricies_valid(matr_weights, b_weights)
+            assert self._is_input_matricies_valid(w_matrices, b_weights)
             self.w_matrices = w_matrices
             self.b_weights = b_weights
         self._assign_activation_funcs()
 
-    def _init_random_weights(self):
+    def _init_random_weights(self) -> None:
         self.w_matrices = []
         self.b_weights = []
 
@@ -42,11 +42,11 @@ class FeedForwardNetwork:
             self.w_matrices.append(self.random_gen.uniform(-1, 1, size=(layers[i-1], layers[i])))
             self.b_weights.append(self.random_gen.uniform(-1, 1, size=layers[i]))
 
-    def _is_input_matricies_valid(self, matr_weights, b_weights):
+    def _is_input_matricies_valid(self, w_matrices: list[ArrayLike], b_weights: list[ArrayLike]) -> bool:
         # TODO
         pass
 
-    def _assign_activation_funcs(self):
+    def _assign_activation_funcs(self) -> None:
         func_map = {'relu': lambda v: np.maximum(0, v),
                     'sigmoid': lambda v: 1.0 / (1.0 + np.exp(-v))}
         self.hidden_activation = func_map[self.hidden_activation]
