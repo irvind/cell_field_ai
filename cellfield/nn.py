67446e437e015878c656406ba258e417dda8c1a9
Eugene Obukhov <irvind25@gmail.com>
2023-06-14 06:12:11 +0300

    Add weights params to network

--- cellfield/nn.py
+++ cellfield/nn.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 import numpy as np
+from numpy.typing import ArrayLike
 
 
 class FeedForwardNetwork:
@@ -9,28 +10,41 @@ class FeedForwardNetwork:
                  output_num: int,
                  hidden_activation: str = 'relu',
                  output_activation: str = 'sigmoid',
-                 seed: Optional[int] = None):
+                 seed: Optional[int] = None,
+                 w_matrices: Optional[List[ArrayLike]] = None,
+                 b_weights: Optional[List[ArrayLike]] = None):
+        assert (matr_weights and b_weights) or (not matr_weights and not b_weights)
+
         self.input_num = input_num
         self.hidden_layer_nums = hidden_layer_nums
         self.output_num = output_num
         self.hidden_activation = hidden_activation
         self.output_activation = output_activation
-        self.rand = np.random.RandomState(seed)
+        self.random_gen = np.random.default_rng(seed)
 
-        self._init_random_weights()
+        if matr_weights:
+            self._init_random_weights()
+        else:
+            assert self._is_input_matricies_valid(matr_weights, b_weights)
+            self.w_matrices = w_matrices
+            self.b_weights = b_weights
         self._assign_activation_funcs()
 
     def _init_random_weights(self):
         self.w_matrices = []
-        self.b_coefs = []
+        self.b_weights = []
 
         layers = [self.input_num]
         layers.extend(self.hidden_layer_nums)
         layers.append(self.output_num)
 
         for i in range(1, len(layers)):
-            self.w_matrices.append(np.random.uniform(-1, 1, size=(layers[i-1], layers[i])))
-            self.b_coefs.append(np.random.uniform(-1, 1, size=layers[i]))
+            self.w_matrices.append(self.random_gen.uniform(-1, 1, size=(layers[i-1], layers[i])))
+            self.b_weights.append(self.random_gen.uniform(-1, 1, size=layers[i]))
+
+    def _is_input_matricies_valid(self, matr_weights, b_weights):
+        # TODO
+        pass
 
     def _assign_activation_funcs(self):
         func_map = {'relu': lambda v: np.maximum(0, v),
@@ -40,7 +54,7 @@ class FeedForwardNetwork:
 
     def feed_forward(self, X: np.ndarray) -> np.ndarray:
         a = X
-        for ind, (w, b) in enumerate(zip(self.w_matrices, self.b_coefs)):
+        for ind, (w, b) in enumerate(zip(self.w_matrices, self.b_weights)):
             z = np.dot(a, w) + b
             a = (self.hidden_activation(z)
                  if ind < len(self.w_matrices) - 1
