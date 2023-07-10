b96d51633ae86fc1a8a786ce7cd19c444e62a9ad
Eugene Obukhov <irvind25@gmail.com>
2023-07-10 03:23:37 +0300

    Grab settings from env vars

--- cellfield/settings.py
+++ cellfield/settings.py
@@ -1,5 +1,15 @@
-POPULATION_SIZE = 200
-OFFSPRING_NUMBER = 200
-FPS = 1
-HIDDEN_LAYERS = [20, 12]
-MUTATION_PROBABILITY = 0.25
+import os
+
+
+def get_int_list_from_env(env_var_name, default=None):
+    v = os.getenv(env_var_name)
+    if not v:
+        return default
+    
+    return [int(token.strip()) for token in v.split(',')]
+
+
+POPULATION_SIZE = int(os.getenv('POPULATION_SIZE', 200))
+OFFSPRING_NUMBER = int(os.getenv('OFFSPRING_NUMBER', 200))
+HIDDEN_LAYERS = get_int_list_from_env('HIDDEN_LAYERS', [20, 12]) 
+MUTATION_PROBABILITY = float(os.getenv('MUTATION_PROBABILITY', 0.25)) 
